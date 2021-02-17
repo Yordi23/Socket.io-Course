56531c823b14c220954fb72ca635dc41eff3bb31
Yordi Ogando <yordiogando@gmail.com>
2021-02-17 17:07:54 -0400

    Initial commit

---
+++ 01-socker-server-basic/index.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+const server = require("http").createServer(app);
+
+const io = require("socket.io")(server);
+
+app.use(express.static(__dirname + "/public"));
+
+io.on("connection", () => {
+  console.log("Client connected");
+});
+
+server.listen(8080, () => {
+  console.log("Server running on port 8080...");
+});
