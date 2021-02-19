fd7a1349cfecab7b3e46d1f651da33e39d44da0b
Yordi Ogando <yordiogando@gmail.com>
2021-02-18 23:12:53 -0400

    Created Band and BandList classes

---
+++ 02-band-names-app/band-names-server/models/band-list.js
@@ -0,0 +1,45 @@
+const Band = require("./band");
+
+class BandList {
+  constructor() {
+    this.bands = [
+      new Band("Metallica"),
+      new Band("Linkin Park"),
+      new Band("Green Day"),
+      new Band("One Republic"),
+    ];
+  }
+
+  createBand(name) {
+    const newBand = new Band(name);
+    this.bands.push(newBand);
+
+    return this.bands;
+  }
+
+  deleteBand(id) {
+    this.bands = this.bands.filter((band) => band.id !== id);
+  }
+
+  getAllBands() {
+    return this.band;
+  }
+
+  voteUp(id) {
+    this.bands = this.bands.map((band) => {
+      if (band.id === id) band.votes += 1;
+
+      return band;
+    });
+  }
+
+  updateBandName(id, newName) {
+    this.bands = this.bands.map((band) => {
+      if (band.id === id) band.name = newName;
+
+      return band;
+    });
+  }
+}
+
+module.exports = BandList;
---
+++ 02-band-names-app/band-names-server/models/band.js
@@ -0,0 +1,10 @@
+const { v4: uuid } = require("uuid");
+class Band {
+  constructor(name) {
+    this.id = uuid();
+    this.name = name;
+    this.votes = 0;
+  }
+}
+
+module.exports = Band;
