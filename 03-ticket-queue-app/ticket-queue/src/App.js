5b8039889fadc69623a54ad624f46155e4a01a0f
Yordi Ogando <yordiogando@gmail.com>
2021-02-23 20:12:23 -0400

    Created Queue Application pages

--- 03-ticket-queue-app/ticket-queue/src/App.js
+++ 03-ticket-queue-app/ticket-queue/src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import { RouterPage } from './pages/RouterPage';
 
 function App() {
   return (
     <div>
-      Hello World
+      <RouterPage/>
     </div>
   );
 }
---
+++ 03-ticket-queue-app/ticket-queue/src/pages/QueuePage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const QueuePage = () => {
+    return (
+        <div>
+            Queue Page
+        </div>
+    )
+}
