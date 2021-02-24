496f17fa4cac273f9aaa6d9f1b9a21211bf129a7
Yordi Ogando <yordiogando@gmail.com>
2021-02-23 20:40:05 -0400

    Implemented pages router

--- 03-ticket-queue-app/ticket-queue/src/pages/RouterPage.js
+++ 03-ticket-queue-app/ticket-queue/src/pages/RouterPage.js
@@ -5,42 +5,73 @@ import {
   VideoCameraOutlined,
   UploadOutlined,
 } from '@ant-design/icons';
+import {
+  Link,
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import { DesktopJoinPage } from './DesktopJoinPage';
+import { QueuePage } from './QueuePage';
+import { TicketCreationPage } from './TicketCreationPage';
+import { DesktopPage } from './DesktopPage';
 
 const { Sider, Content } = Layout;
 
 export const RouterPage = () => {
   return (
-    <Layout style={{ height: '100vh' }}>
-      <Sider>
-        <div className="logo" />
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['1']}
-        >
-          <Menu.Item key="1" icon={<UserOutlined />}>
-            Join Desktop
-          </Menu.Item>
-          <Menu.Item key="2" icon={<VideoCameraOutlined />}>
-            Queue
-          </Menu.Item>
-          <Menu.Item key="3" icon={<UploadOutlined />}>
-            Create Ticket
-          </Menu.Item>
-        </Menu>
-      </Sider>
-      <Layout className="site-layout">
-        <Content
-          className="site-layout-background"
-          style={{
-            margin: '24px 16px',
-            padding: 24,
-            minHeight: 280,
-          }}
-        >
-          Content
-        </Content>
+    <Router>
+      <Layout style={{ height: '100vh' }}>
+        <Sider>
+          <div className="logo" />
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+          >
+            <Menu.Item key="1" icon={<UserOutlined />}>
+              <Link to="/join-desktop">Join Desktop</Link>
+            </Menu.Item>
+            <Menu.Item
+              key="2"
+              icon={<VideoCameraOutlined />}
+            >
+              <Link to="/queue">Queue</Link>
+            </Menu.Item>
+            <Menu.Item key="3" icon={<UploadOutlined />}>
+              <Link to="/create-ticket">Create Ticket</Link>
+            </Menu.Item>
+          </Menu>
+        </Sider>
+        <Layout className="site-layout">
+          <Content
+            className="site-layout-background"
+            style={{
+              margin: '24px 16px',
+              padding: 24,
+              minHeight: 280,
+            }}
+          >
+            <Switch>
+              <Route
+                path="/join-desktop"
+                component={DesktopJoinPage}
+              />
+              <Route path="/queue" component={QueuePage} />
+              <Route
+                path="/create-ticket"
+                component={TicketCreationPage}
+              />
+              <Route
+                path="/desktop"
+                component={DesktopPage}
+              />
+              <Redirect to="/join-desktop" />
+            </Switch>
+          </Content>
+        </Layout>
       </Layout>
-    </Layout>
+    </Router>
   );
 };
