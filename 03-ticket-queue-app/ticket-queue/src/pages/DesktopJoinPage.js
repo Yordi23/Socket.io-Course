aebe7f647c6db7ef2ccce2a1a234db8d3566ecb8
Yordi Ogando <yordiogando@gmail.com>
2021-02-23 21:21:31 -0400

    Worked on DesktopnJoinPage and Desktop page

--- 03-ticket-queue-app/ticket-queue/src/pages/DesktopJoinPage.js
+++ 03-ticket-queue-app/ticket-queue/src/pages/DesktopJoinPage.js
@@ -1,9 +1,101 @@
-import React from 'react'
+import React from 'react';
+import {
+  Form,
+  Input,
+  Button,
+  InputNumber,
+  Typography,
+  Divider,
+} from 'antd';
+import { SaveOutlined } from '@ant-design/icons';
+import { useHistory } from 'react-router-dom';
+
+const { Title, Text } = Typography;
+const layout = {
+  labelCol: {
+    span: 4,
+  },
+  wrapperCol: {
+    span: 14,
+  },
+};
+const tailLayout = {
+  wrapperCol: {
+    offset: 4,
+    span: 14,
+  },
+};
 
 export const DesktopJoinPage = () => {
-    return (
-        <div>
-            DesktopJoinPage
-        </div>
-    )
-}
+  const history = useHistory();
+
+  const onFinish = (values) => {
+    console.log('Success:', values);
+
+    history.push('/desktop');
+  };
+
+  const onFinishFailed = (errorInfo) => {
+    console.log('Failed:', errorInfo);
+  };
+
+  return (
+    <>
+      <Title level={2}>Join</Title>
+      <Text>Enter your name and desktop number</Text>
+      <Divider />
+      <Form
+        {...layout}
+        name="basic"
+        initialValues={{
+          remember: true,
+        }}
+        onFinish={onFinish}
+        onFinishFailed={onFinishFailed}
+      >
+        <Form.Item
+          label="Name"
+          name="name"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your name!',
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+
+        <Form.Item
+          label="Desktop"
+          name="desktop"
+          rules={[
+            {
+              required: true,
+              message: 'Please input your desktop number!',
+            },
+          ]}
+        >
+          <InputNumber min={1} max={99} />
+        </Form.Item>
+
+        <Form.Item
+          {...tailLayout}
+          name="remember"
+          valuePropName="checked"
+        ></Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button
+            type="primary"
+            htmlType="submit"
+            shape="round"
+          >
+            <SaveOutlined />
+            Join
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
+  );
+};
--- 03-ticket-queue-app/ticket-queue/src/pages/DesktopPage.js
+++ 03-ticket-queue-app/ticket-queue/src/pages/DesktopPage.js
@@ -1,9 +1,65 @@
-import React from 'react'
+import {
+  ClockCircleOutlined,
+  RightOutlined,
+} from '@ant-design/icons';
+import {
+  Button,
+  Col,
+  Divider,
+  Row,
+  Typography,
+} from 'antd';
+import React from 'react';
+
+const { Title, Text } = Typography;
 
 export const DesktopPage = () => {
-    return (
-        <div>
-            Desktop Page
-        </div>
-    )
-}
+  const exit = () => {};
+  const nextTicket = () => {};
+  return (
+    <>
+      <Row>
+        <Col span={20}>
+          <Title level={2}>User</Title>
+          <Text>You are working on the desk number: </Text>
+          <Text type="success">5</Text>
+        </Col>
+
+        <Col span={4} align="right">
+          <Button
+            shape="round"
+            type="danger"
+            onClick={exit}
+          >
+            <ClockCircleOutlined />
+            Exit
+          </Button>
+        </Col>
+      </Row>
+
+      <Divider />
+
+      <Row>
+        <Col>
+          <Text>You are attending ticket number:</Text>
+          <Text style={{ fontSize: 30 }} type="danger">
+            55
+          </Text>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col offset={18} span={6} align="right">
+          <Button
+            shape="round"
+            type="primary"
+            onClick={nextTicket}
+          >
+            <RightOutlined />
+            Next
+          </Button>
+        </Col>
+      </Row>
+    </>
+  );
+};
