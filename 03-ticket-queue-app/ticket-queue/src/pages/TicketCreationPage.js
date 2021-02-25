bf637e766a995a071e34870e2bd5bb2533026cb0
Yordi Ogando <yordiogando@gmail.com>
2021-02-24 23:15:08 -0400

    Initial structure for QueuePage and TicketCreation page

--- 03-ticket-queue-app/ticket-queue/src/pages/TicketCreationPage.js
+++ 03-ticket-queue-app/ticket-queue/src/pages/TicketCreationPage.js
@@ -1,9 +1,40 @@
-import React from 'react'
+import { DownloadOutlined } from '@ant-design/icons';
+import { Row, Col, Typography, Button } from 'antd';
+import React from 'react';
+
+const { Title, Text } = Typography;
 
 export const TicketCreationPage = () => {
-    return (
-        <div>
-            Ticket Creation Page
-        </div>
-    )
-}
+  const newTicket = () => {};
+  return (
+    <>
+      <Row>
+        <Col span={14} offset={6} align="center">
+          <Title level={3}>
+            Press button to create new ticket
+          </Title>
+
+          <Button
+            type="primary"
+            shape="round"
+            icon={<DownloadOutlined />}
+            size="large"
+            onClick={newTicket}
+          >
+            New Ticket
+          </Button>
+        </Col>
+      </Row>
+
+      <Row style={{ marginTop: 100 }}>
+        <Col span={14} offset={6} align="center">
+          <Text level={2}>Your number</Text>
+          <br />
+          <Text type="success" style={{ fontSize: 55 }}>
+            23
+          </Text>
+        </Col>
+      </Row>
+    </>
+  );
+};
