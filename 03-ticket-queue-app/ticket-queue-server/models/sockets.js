b00ea09ca9ccc78e6c7c884976499256a79898a1
Yordi Ogando <yordiogando@gmail.com>
2021-02-25 18:28:25 -0400

    Initial implementation of ticket queue server

---
+++ 03-ticket-queue-app/ticket-queue-server/models/sockets.js
@@ -0,0 +1,40 @@
+const TicketList = require('./ticket-list');
+
+class Sockets {
+  constructor(io) {
+    this.io = io;
+    this.bandList = new TicketList();
+
+    this.socketEvents();
+  }
+
+  socketEvents() {
+    this.io.on('connection', (socket) => {
+      console.log('Client conected');
+
+      // socket.emit('current-bands', this.bandList.getAllBands());
+
+      // socket.on('vote-band', (data) => {
+      //   this.bandList.voteUp(data.id);
+      //   this.io.emit('current-bands', this.bandList.getAllBands());
+      // });
+
+      // socket.on('delete-band', (data) => {
+      //   this.bandList.deleteBand(data.id);
+      //   this.io.emit('current-bands', this.bandList.getAllBands());
+      // });
+
+      // socket.on('update-band-name', (data) => {
+      //   this.bandList.updateBandName(data.id, data.name);
+      //   this.io.emit('current-bands', this.bandList.getAllBands());
+      // });
+
+      // socket.on('create-band', (data) => {
+      //   this.bandList.createBand(data.name);
+      //   this.io.emit('current-bands', this.bandList.getAllBands());
+      // });
+    });
+  }
+}
+
+module.exports = Sockets;
---
+++ 03-ticket-queue-app/ticket-queue-server/models/ticket-list.js
@@ -0,0 +1,39 @@
+const Ticket = require('./ticket');
+
+class TicketList {
+  constructor() {
+    this.lastNumber = 0;
+    this.pendingTickets = [];
+    this.assignedTickets = [];
+  }
+
+  get nextNumber() {
+    return ++this.lastNumber;
+  }
+
+  get topTickets() {
+    return this.assignedTickets.slice(0, 13);
+  }
+
+  createTicket() {
+    const newTicket = new Ticket(this.lastNumber);
+    this.pendingTickets.push(newTicket);
+
+    return newTicket;
+  }
+
+  assignTicket(agent, desktop) {
+    if (this.pendingTickets.length) return undefined;
+    if (!agent || !desktop) return undefined;
+
+    const nextTicket = this.pendingTickets.shift();
+    nextTicket.agent = agent;
+    nextTicket.desktop = desktop;
+
+    this.assignedTickets.unshift(nextTicket);
+
+    return nextTicket;
+  }
+}
+
+module.exports = TicketList;
