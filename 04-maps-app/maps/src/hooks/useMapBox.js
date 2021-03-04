524a7eca9f85ccc460d2eedbabeeefea4d4d91e8
Yordi Ogando <yordiogando@gmail.com>
2021-03-04 18:52:12 -0400

    Implemented create marker funtionality

--- 04-maps-app/maps/src/hooks/useMapBox.js
+++ 04-maps-app/maps/src/hooks/useMapBox.js
@@ -1,5 +1,6 @@
 import mapboxgl from 'mapbox-gl';
 import { useCallback, useEffect, useRef, useState } from 'react';
+import { v4 as uuid } from 'uuid';
 
 mapboxgl.accessToken = 'pk.eyJ1IjoieW9yZGkyMyIsImEiOiJja2x1NWo2eHQwY3gyMnZvNHhmOTN1d2l0In0.4589zX5E8pybw8FFCrWA-A';
 
@@ -8,6 +9,8 @@ export const useMapBox = (startPoint) => {
     const setRef = useCallback((node) => {
         mapDiv.current = node;
     }, []);
+
+    const markers = useRef({});
 
     const map = useRef();
     const [coords, setCoords] = useState(startPoint);
@@ -37,8 +40,24 @@ export const useMapBox = (startPoint) => {
         });
     }, []);
 
+    useEffect(() => {
+        map.current.on('click', (e) => {
+            const { lng, lat } = e.lngLat;
+
+            const marker = new mapboxgl.Marker();
+            marker.id = uuid();
+
+            marker.setLngLat([lng, lat])
+                .addTo(map.current)
+                .setDraggable(true);
+
+            markers.current[marker.id] = marker;
+        });
+    }, []);
+
     return {
         coords,
+        markers,
         setRef
     };
 };
