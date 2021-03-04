dd620c400f5c41cdb87efc599fe7b01108a8c2ab
Yordi Ogando <yordiogando@gmail.com>
2021-03-04 18:31:36 -0400

    Refactored MapBox logic to custom hook

---
+++ 04-maps-app/maps/src/hooks/useMapBox.js
@@ -0,0 +1,44 @@
+import mapboxgl from 'mapbox-gl';
+import { useCallback, useEffect, useRef, useState } from 'react';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoieW9yZGkyMyIsImEiOiJja2x1NWo2eHQwY3gyMnZvNHhmOTN1d2l0In0.4589zX5E8pybw8FFCrWA-A';
+
+export const useMapBox = (startPoint) => {
+    const mapDiv = useRef();
+    const setRef = useCallback((node) => {
+        mapDiv.current = node;
+    }, []);
+
+    const map = useRef();
+    const [coords, setCoords] = useState(startPoint);
+
+    useEffect(() => {
+        const newMap = new mapboxgl.Map({
+            container: mapDiv.current,
+            style: 'mapbox://styles/mapbox/streets-v11',
+            center: [startPoint.lng, startPoint.lat],
+            zoom: startPoint.zoom
+        });
+
+        map.current = newMap;
+
+    }, [startPoint]);
+
+    useEffect(() => {
+        map.current.on('move', () => {
+            const { lng, lat } = map.current.getCenter();
+            const zoom = map.current.getZoom();
+
+            setCoords({
+                lng: lng.toFixed(4),
+                lat: lat.toFixed(4),
+                zoom: zoom.toFixed(2),
+            });
+        });
+    }, []);
+
+    return {
+        coords,
+        setRef
+    };
+};
--- 04-maps-app/maps/src/pages/MapPage.js
+++ 04-maps-app/maps/src/pages/MapPage.js
@@ -1,7 +1,6 @@
 import React, { useEffect, useRef, useState } from 'react';
-import mapboxgl from 'mapbox-gl';
+import { useMapBox } from '../hooks/useMapBox';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoieW9yZGkyMyIsImEiOiJja2x1NWo2eHQwY3gyMnZvNHhmOTN1d2l0In0.4589zX5E8pybw8FFCrWA-A';
 const startPoint = {
 	lng: 5,
 	lat: 32,
@@ -9,41 +8,14 @@ const startPoint = {
 };
 
 export const MapPage = () => {
-	const mapDiv = useRef();
-	const map = useRef();
-	const [coords, setCoords] = useState(startPoint);
-
-	useEffect(() => {
-		const newMap = new mapboxgl.Map({
-			container: mapDiv.current,
-			style: 'mapbox://styles/mapbox/streets-v11',
-			center: [startPoint.lng, startPoint.lat],
-			zoom: startPoint.zoom
-		});
-
-		map.current = newMap;
-
-	}, []);
-
-	useEffect(() => {
-		map.current.on('move', () => {
-			const { lng, lat } = map.current.getCenter();
-			const zoom = map.current.getZoom();
-
-			setCoords({
-				lng: lng.toFixed(4),
-				lat: lat.toFixed(4),
-				zoom: zoom.toFixed(2),
-			});
-		});
-	}, []);
+	const { coords, setRef } = useMapBox(startPoint);
 
 	return <>
 		<div className='info'>
 			Lng: {coords.lng} | Lat: {coords.lat} | Zoom: {coords.zoom}
 		</div>
 		<div
-			ref={mapDiv}
+			ref={setRef}
 			className="mapContainer">
 
 		</div>
